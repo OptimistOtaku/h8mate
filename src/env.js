4ac924e2acbc0335d7218538c5e30be6b23cb2d0
OptimistOtaku <optimistotaku@gmail.com>
2025-03-29 19:43:44 +0530

    Update env.js

--- src/env.js
+++ src/env.js
@@ -6,17 +6,28 @@ export const env = createEnv({
     AUTH_SECRET: z.string().optional(),
     AUTH_DISCORD_ID: z.string().optional(),
     AUTH_DISCORD_SECRET: z.string().optional(),
-    DATABASE_URL: z.string().url().optional(),
-    NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
+
+    DATABASE_URL:
+      process.env.NODE_ENV === "production"
+        ? z.string().url().default("https://example.com")
+        : z.string().optional(),
+
+    NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
   },
+
   client: {},
+
   runtimeEnv: {
     AUTH_SECRET: process.env.AUTH_SECRET,
     AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID,
     AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
-    DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_URL: process.env.DATABASE_URL ?? "https://example.com",
     NODE_ENV: process.env.NODE_ENV,
   },
+
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
   emptyStringAsUndefined: true,
 });
+
